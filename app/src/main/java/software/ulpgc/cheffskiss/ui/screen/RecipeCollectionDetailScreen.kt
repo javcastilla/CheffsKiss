f859588b1d0ff78b53b4e8e52b80f397b4e7893a
Dani <164520978+DanGutRec@users.noreply.github.com>
2026-05-16 14:09:04 +0100

    Renamed file

--- app/src/main/java/software/ulpgc/cheffskiss/ui/screen/RecipeCollectionDetailScreen.kt
+++ app/src/main/java/software/ulpgc/cheffskiss/ui/screen/RecipeCollectionDetailScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,7 +22,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import software.ulpgc.cheffskiss.domain.model.Recipe
-import software.ulpgc.cheffskiss.domain.model.RecipeCollection
 import software.ulpgc.cheffskiss.ui.RecipeCollectionDetailViewModel
 import software.ulpgc.cheffskiss.ui.theme.*
 
